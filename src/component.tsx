b574a2f8684930ad5982f5dec5458050bd2e6ff1
Karl Bailey <baika654@student.otago.ac.nz>
2021-06-14 08:18:49 +1200

    Files for keytar experiment

--- src/component.tsx
+++ src/component.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import ElectronApp from './ElectronApp'
 
 function App() {
   return <h1>Hello, react!</h1>
 }
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<ElectronApp />, document.getElementById("app"))
